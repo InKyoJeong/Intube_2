4e8f433fbff9f27cfa1a84e05327444e8d99ff5e
InKyoJeong <viapolar@naver.com>
2020-04-26 23:23:58 +0900

    feat: Add middlewares and Split init

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from Home");
+
+const handelProfile = (req, res) => res.send("Your Pofile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", handleHome);
+
+app.get("/profile", handelProfile);
+
+export default app;
