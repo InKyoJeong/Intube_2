dffc60561e9a8103da6e6e5ae6c7e34df2ca9995
InKyoJeong <viapolar@naver.com>
2020-04-29 23:58:47 +0900

    docs: Add pug files

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 app.use(helmet());
+app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
