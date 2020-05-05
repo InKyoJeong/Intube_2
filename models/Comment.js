6f51580ff36248b362da673012a4ec65c8d33a7d
InKyoJeong <viapolar@naver.com>
2020-05-05 16:24:52 +0900

    feat: Add models - video, comment

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: "Text is required",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const model = mongoose.model("Commnet", CommentSchema);
+
+export default model;
