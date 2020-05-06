d8b4b3cc4e16a1b3107256d88b98afd702199e10
InKyoJeong <viapolar@naver.com>
2020-05-07 00:15:30 +0900

    fix: Modify videoFile to fileUrl

--- controller/videoController.js
+++ controller/videoController.js
@@ -21,12 +21,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
-  const { body, file } = req;
-  console.log(body, file);
-  //To Do : Upload and save video
-  // res.redirect(routes.videoDetail(324393));
-  res.render("upload", { pageTitle: "Upload" });
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path },
+  } = req;
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
